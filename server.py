956f444b0cc7c7bccedc05450a4c2496b0c030ca
Kons <kons0@libero.it>
2019-02-16 12:05:04 +0100

    [server] Use binary based consistent communication

--- server.py
+++ server.py
@@ -2,10 +2,13 @@
 
 import socket
 import ads1256
+import struct
 
 TCP_IP = '192.168.1.104'
 TCP_PORT = 5005
-BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+
+TCP_PACK_SIZE = 150
+TCP_SAMPLE_SIZE = 8
 
 #Start the ADC
 ads1256.start("1","2d5")
@@ -19,14 +22,24 @@ print "Wait for client..."
 conn, addr = s.accept()
 print 'Connection address:', addr
 while 1:
-	#data = conn.recv(BUFFER_SIZE)
-	#if not data: break
-	#print "received data:", data
+	#outMsg = ""
+	outBin = ""
 	
-	data = ads1256.read_channel(2)
-	#print "sending :", data
+	for i in range(TCP_PACK_SIZE):
+		data1 = ads1256.read_channel(2)
+		data2 = ads1256.read_channel(3)
+		if data1 == 0 or data2 == 0:
+			print "ZERO %s %s"%(data1,data2)
+		#data3 = ads1256.read_channel(4) -> We need only 2
+		#Append data to the outgoing message
+		#outMsg = outMsg + "%s,%s:"%(data1,data2)
+		outBin = outBin + struct.pack('!ii', data1, data2)
+		
 	
-	conn.send(str(data) + ":")
+	#print "sending :", outMsg
+	#Send aggregated data
+	#conn.send(outMsg)
+	conn.send(outBin)
 	
 
 ads1256.stop()
